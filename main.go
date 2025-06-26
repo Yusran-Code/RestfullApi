427c0b7c8c165ea538181417cadf72fcb3eea982
LuckyCode <yyussrraann1992@gmail.com>
2025-06-26 21:16:49 +0800

    add error handling

--- main.go
+++ main.go
@@ -13,16 +13,14 @@ import (
 
 var DB *gorm.DB
 
-
-
-func main()  {
+func main() {
 	dsn := "host=localhost user=postgres password=LuckyCode dbname=data sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn),&gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Connection DB failed",err)
+		log.Fatal("Connection DB failed", err)
 	}
-	
-    DB = db
+
+	DB = db
 	err = DB.AutoMigrate(&models.Product{})
 	if err != nil {
 		log.Fatal("migration failed")
@@ -33,103 +31,101 @@ func main()  {
 	r := gin.Default()
 	r.GET("/product", getProducts)
 	r.GET("/product/:id", getProduct)
-	r.POST("/product",createProduct)
-	r.PUT("/product/:id",updateProduct)
-	r.DELETE("/product/:id",deleteProduct)
+	r.POST("/product", createProduct)
+	r.PUT("/product/:id", updateProduct)
+	r.DELETE("/product/:id", deleteProduct)
 
 	r.Run(":8080")
 }
 
-func getProducts(c *gin.Context)  {
+func getProducts(c *gin.Context) {
 	var products []models.Product
-	if err:= DB.Find(&products).Error; err != nil {
-		c.JSON(500,gin.H{"error": "products not found"})
+	if err := DB.Find(&products).Error; err != nil {
+		c.JSON(500, gin.H{"error": "failed get products "})
 		return
 	}
 
 	c.JSON(200, products)
-	
 
 }
 
-func getProduct(c *gin.Context)  {
+func getProduct(c *gin.Context) {
 	var product models.Product
 	idStr := c.Param("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		c.JSON(400, gin.H{"error": "shdhsgdhsgdsd"})
+		c.JSON(404, gin.H{"error": "Get product Not Found"})
 		return
 	}
-	
-	if err := DB.First(&product,id).Error; err != nil {
-		c.JSON(404,gin.H{"error": "nosdsdsdsdsd"})
+
+	if err := DB.First(&product, id).Error; err != nil {
+		c.JSON(500, gin.H{"error": "Failed Get Id Product"})
 		return
 	}
-  c.JSON(200,product)
+	c.JSON(200, product)
 }
 
-func createProduct(c *gin.Context)  {
+func createProduct(c *gin.Context) {
 	var product models.Product
 	if err := c.ShouldBindJSON(&product); err != nil {
-		c.JSON(400, gin.H{"error":"asasasa"})
-		return 
+		c.JSON(400, gin.H{"error": "failed to save"})
+		return
 	}
 
 	if err := DB.Create(&product); err != nil {
-		c.JSON(400,gin.H{"error": "sdsdsdsddsd"})
-		return 
+		c.JSON(500, gin.H{"error": "failed to Create product"})
+		return
 	}
 
-	c.JSON(200,product)
+	c.JSON(201, product)
 }
 
-func updateProduct(c *gin.Context)  {
+func updateProduct(c *gin.Context) {
 	var product models.Product
 	idStr := c.Param("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		c.JSON(400, gin.H{"error": "sdsdsdssdsdsd"})
-		return 
+		c.JSON(400, gin.H{"error": "failed update id"})
+		return
 	}
 
-	if err := DB.First(&product,id).Error; err != nil {
-		c.JSON(400, gin.H{"error": "erasaresaresraes"})
-		return 
+	if err := DB.First(&product, id).Error; err != nil {
+		c.JSON(500, gin.H{"error": "unknown id update "})
+		return
 	}
 
 	var input models.Product
 	if err := c.ShouldBindJSON(&input); err != nil {
-		c.JSON(300,gin.H{"error": "sdsdsdsdsds"})
+		c.JSON(404, gin.H{"error": "not found input update"})
 		return
 	}
 
 	if err := DB.Model(&product).Updates(&input).Error; err != nil {
-		c.JSON(400,gin.H{"error": "sdsdssdd"})
-		return 
+		c.JSON(500, gin.H{"error": "failed update product"})
+		return
 	}
 
 	c.JSON(200, product)
 }
 
-func deleteProduct(c *gin.Context)  {
+func deleteProduct(c *gin.Context) {
 	var product models.Product
 	idStr := c.Param("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		c.JSON(400, gin.H{"error": "sdsddsd"})
-		return 
+		c.JSON(404, gin.H{"error": "not found id delete"})
+		return
 	}
 
-	if  err:= DB.First(&product,id).Error; err != nil {
+	if err := DB.First(&product, id).Error; err != nil {
 		c.JSON(401, gin.H{"error": "asasasas"})
-		return 
+		return
 	}
 
-	if err := DB.Delete(&product,id).Error; err != nil {
-		c.JSON(400, gin.H{"error": "sdsdsdsdsd"})
-		return 
+	if err := DB.Delete(&product, id).Error; err != nil {
+		c.JSON(500, gin.H{"error": "failed delete product"})
+		return
 	}
 	c.JSON(200, product)
-
 
 }
